9ebbfdef35e5218a0376b25ac6db35c6fc522a4b
jon-jh <hiebert456@gmail.com>
2024-10-08 09:42:40 -0600

    Move sample data to App.js, pass it down to PhotoListItem as (props)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
+
+//Move the sample data here (this is the parent component which we will use to pass data down to the child components)
+
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem />
+      <PhotoListItem data = {sampleDataForPhotoListItem}/>
       We rendering now
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,21 +2,13 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
+
+const PhotoListItem = (props) => {
   /* Insert React */
+  // After moving it to app.js, it became (props)
   // Destructure the sample data object
-  const { id, location, imageSource, username, profile } = sampleDataForPhotoListItem;
+  const { id, location, imageSource, username, profile } = props.data;
   
   return (
     <div>
