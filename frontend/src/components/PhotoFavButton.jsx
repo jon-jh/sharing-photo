6c1a9dca0d7f90b0cb83317a645e3df2879dec6d
jon-jh <hiebert456@gmail.com>
2024-10-09 20:01:46 -0600

    Add the Favorite Button (no onClick handler yet)

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,11 +3,16 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+function PhotoFavButton(props) {
+  const [favButton, toggleFavState] = useState(false)
+  const handleFav = () => {
+    favButton ? toggleFavState(false ) : toggleFavState(true)
+  }
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={handleFav}>
       <div className="photo-list__fav-icon-svg">
         {/* Insert React */}
+        <FavIcon selected={favButton}></FavIcon>
       </div>
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
-  const { username, imageSource, id, location, profile } = props.photoData;
+  const { username, imageSource, id, location, profile, index } = props.photoData;
+
   return (
     <div>
+      <div className="photo-list__item" key={id}>
+        <PhotoFavButton key={id} />
       <img className="photo-list__image" src={imageSource} alt="Photo" />
       <div className="photo-list__user-details">
-        <img className="photo-list__user-profile" src={profile} alt="Profile" />
+        <img className="photo-list__user-profile" src={profile} />
         <div className="photo-list__user-info">
           <p>{username}</p>
           <p className="photo-list__user-location">{location.city}, {location.country}</p>
         </div>
       </div>
+    </div>
     </div>
   );
 };
