5fd72515d20473410ee2476950920f94e0f6f839
jon-jh <hiebert456@gmail.com>
2024-10-09 22:50:26 -0600

    Note. The useEffect hook runs any time favoritesArray changes. When favoritesArray changes, the useEffect hook updates isFavPhoto using setIsFavPhotoExist.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,25 +5,34 @@ import photos from "mocks/photos";
 import topics from 'mocks/topics';
 
 const App = () => {
-  const [isFavPhoto, isFavPhotoExist] = useState(false);
-  const [favoritesArray, modifyfavoritesArray] = useState([]);
+  const [isFavPhoto, setIsFavPhotoExist] = useState(false);
+  const [favoritesArray, modifyFavoritesArray] = useState([]);
 
   const addOrRemoveFav = (id, state) => {
     state
-      ? modifyfavoritesArray(currentFavs => currentFavs.filter(fav => Number(fav.id) !== Number(id) + 1))
-      : modifyfavoritesArray(currentFavs => [...currentFavs, photos[id]]);
+      ? modifyFavoritesArray(currentFavs => currentFavs.filter(fav => Number(fav.id) !== Number(id) + 1))
+      : modifyFavoritesArray(currentFavs => [...currentFavs, photos[id]]);
   };
 
   useEffect(() => {
     console.log('Favorite Photos Array:', favoritesArray);
+    setIsFavPhotoExist(favoritesArray.length > 0);
   }, [favoritesArray]);
+  
+  // notes for future reference
+  // isFavPhotoExist is now being passed from and controlled by setIsFavPhotoExist
+  // the useEffect hook runs any time favoritesArray changes
+  // When favoritesArray changes, the useEffect hook updates isFavPhoto using setIsFavPhotoExist.
+  // it updates isFavPhoto to true if favoritesArray has at least 1 item
+  // in the css for FavBadge which imports the FavIcon, if isFavPhotoExist is true it will display the notification icon, else it will not.
+  
 
   return (
     <div className="App">
       <HomeRoute
         topics={topics}
         photos={photos}
-        isFavPhotoExist={isFavPhotoExist}
+        isFavPhotoExist={setIsFavPhotoExist}
         isFavPhoto={isFavPhoto}
         addOrRemoveFav={addOrRemoveFav}
       />
