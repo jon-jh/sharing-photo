3057332061b0fd9d54f967e2828ac6010d443ea5
jon-jh <hiebert456@gmail.com>
2024-10-10 08:48:38 -0600

    Console log the details being passed to PhotoDetailsModal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -5,6 +5,9 @@ import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = (props) => {
   const { photo, handleCloseButton } = props;
+  // Log the photo details to the console
+  console.log(photo);
+  
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={handleCloseButton}><img src={closeSymbol} alt="close symbol" />
