39bd413e509f66b2dac245b79294c49973fcae76
jon-jh <hiebert456@gmail.com>
2024-10-10 08:32:38 -0600

    Move the onClick handler for the close button from the image to the actual button

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -7,8 +7,7 @@ const PhotoDetailsModal = (props) => {
   const { photo, handleCloseButton } = props;
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" onClick={handleCloseButton}/>
+      <button className="photo-details-modal__close-button" onClick={handleCloseButton}><img src={closeSymbol} alt="close symbol" />
       </button>
     </div>
   )
