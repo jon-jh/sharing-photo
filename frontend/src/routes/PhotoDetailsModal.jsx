510aa171ab33ac289ac1b7c42f89512496164400
jon-jh <hiebert456@gmail.com>
2024-10-10 09:28:32 -0600

    Update the styling inside the PhotoDetailsModal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -4,28 +4,34 @@ import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = (props) => {
   const { photo, handleCloseButton } = props;
-  const { id, user: { name: username, profile }, urls: { full: imageSource }, location: { city, country } } = photo;
+  const {
+    id,
+    user: { name: username, profile },
+    urls: { full: imageSource },
+    location: { city, country }
+  } = photo;
 
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={handleCloseButton}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
-      <div className="photo-details-modal__content">
-        <div className="photo-details-modal__images">
-          <img className="photo-details-modal__image" src={imageSource} alt="Photo" />
-        </div>
-        <div className="photo-details-modal__header">
-          <img className="photo-details-modal__profile" src={profile} alt="User Profile" />
-          <div className="photo-details-modal__info">
-            <p>ID: {id}</p>
-            <p>Username: {username}</p>
-            <p>Location: {city}, {country}</p>
+      <img className="photo-details-modal__image" src={imageSource} alt={id} />
+      <div className="photo-details-modal__header">
+        <div className="photo-details-modal__photographer-details">
+          <img className="photo-details-modal__photographer-profile" src={profile} alt="User Profile" />
+          <div className="photo-details-modal__photographer-info">
+            <p>{username}</p>
+            <p className="photo-details-modal__photographer-location">{city}, {country}</p>
           </div>
         </div>
+      </div>
+      <div className="photo-details-modal__images">
       </div>
     </div>
   );
 };
 
 export default PhotoDetailsModal;
+
+
