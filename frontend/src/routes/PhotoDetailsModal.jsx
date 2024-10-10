fc1f2752b9b8b64a1ab1065d9560f9767046ea49
jon-jh <hiebert456@gmail.com>
2024-10-10 09:19:19 -0600

    Photo object is passed from App and destructured in PhotoDetailsModal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
-
-import '../styles/PhotoDetailsModal.scss'
+import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = (props) => {
   const { photo, handleCloseButton } = props;
-  // Log the photo details to the console
-  console.log(photo);
-  
+  const { id, user: { name: username, profile }, urls: { full: imageSource }, location: { city, country } } = photo;
+
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick={handleCloseButton}><img src={closeSymbol} alt="close symbol" />
+      <button className="photo-details-modal__close-button" onClick={handleCloseButton}>
+        <img src={closeSymbol} alt="close symbol" />
       </button>
+      <div className="photo-details-modal__content">
+        <div className="photo-details-modal__images">
+          <img className="photo-details-modal__image" src={imageSource} alt="Photo" />
+        </div>
+        <div className="photo-details-modal__header">
+          <img className="photo-details-modal__profile" src={profile} alt="User Profile" />
+          <div className="photo-details-modal__info">
+            <p>ID: {id}</p>
+            <p>Username: {username}</p>
+            <p>Location: {city}, {country}</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default PhotoDetailsModal;
