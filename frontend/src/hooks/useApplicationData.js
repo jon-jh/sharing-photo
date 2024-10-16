e7064b6a333bc150cb7cf9cd709a6d183bc0bf85
jon-jh <hiebert456@gmail.com>
2024-10-16 15:28:06 -0600

    General clean-up

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,38 +1,42 @@
-import { useReducer, useEffect } from "react";
-import photos from "mocks/photos";
-import { SET_SELECTED_PHOTO, CLOSE_PHOTO, ADD_FAVORITE, REMOVE_FAVORITE, SET_PHOTO_DATA } from './actionTypes';
 import axios from "axios";
+import { useReducer, useEffect } from "react";
+import {
+  SET_SELECTED_PHOTO,
+  CLOSE_PHOTO,
+  ADD_FAVORITE,
+  REMOVE_FAVORITE,
+  SET_PHOTO_DATA
+} from './actionTypes';
 
 const initialState = {
+  photoSelected: null,
   navAtLeastOneFavCheck: false,
   favoritesArray: [],
-  photoSelected: null,
   photos: [],
-  topicData: []
+  topics: []
 };
 
-
-
+// Reducer Function
 function reducer(state, action) {
   switch (action.type) {
 
+    // Load photos from API:
     case SET_PHOTO_DATA:
-      return { ...state, photos: action.payload}
-    
-    
+      return { ...state, photos: action.payload }
+
     case SET_SELECTED_PHOTO:
       return { ...state, photoSelected: action.payload };
+
     case CLOSE_PHOTO:
       return { ...state, photoSelected: null };
+
     case ADD_FAVORITE:
       const updatedFavsAdd = [...state.favoritesArray, photos[action.payload]];
       return { ...state, favoritesArray: updatedFavsAdd, navAtLeastOneFavCheck: updatedFavsAdd.length > 0 };
+
     case REMOVE_FAVORITE:
       const updatedFavsRemove = state.favoritesArray.filter(fav => Number(fav.id) !== Number(action.payload) + 1);
       return { ...state, favoritesArray: updatedFavsRemove, navAtLeastOneFavCheck: updatedFavsRemove.length > 0 };
-
-    
-
 
     default:
       throw new Error('Unknown action type');
@@ -63,20 +67,20 @@ const useApplicationData = () => {
     }
   };
 
+  // Debug Logs
   useEffect(() => {
     console.log('favoritesArray', state.favoritesArray);
     console.log('navAtLeastOneFavCheck', state.navAtLeastOneFavCheck);
-    //debug here later
   });
 
+  // API Load
   useEffect(() => {
     axios.get('http://localhost:8001/api/photos')
       .then((response) => {
-        console.log('axios', response.data)
+        // console.log('axios', response.data)
         dispatch({ type: SET_PHOTO_DATA, payload: response.data });
       })
   }, []);
-
 
   return {
     navAtLeastOneFavCheck: state.navAtLeastOneFavCheck,
